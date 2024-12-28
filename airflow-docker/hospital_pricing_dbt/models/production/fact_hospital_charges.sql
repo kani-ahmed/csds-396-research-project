0cd95c7b722ca4fa33f251fe7965fe274274e757
kenny-ahmedd <kenny.ahmedd@gmail.com>
2024-12-28 02:59:32 -0500

    latest updates

--- airflow-docker/hospital_pricing_dbt/models/production/fact_hospital_charges.sql
+++ airflow-docker/hospital_pricing_dbt/models/production/fact_hospital_charges.sql
@@ -18,11 +18,11 @@ SELECT
     hc.description,
     hc.gross_charge,
     hc.payer_allowed_amount
-FROM {{ ref('hospital_charges') }} hc
-JOIN {{ ref('fact_cpt_codes') }} fc ON hc.cpt_id = fc.cpt_id
-JOIN {{ ref('fact_payers') }} fp ON hc.payer_id = fp.payer_id
-JOIN {{ ref('fact_hospitals') }} fh ON hc.hospital_id = fh.hospital_id
-JOIN {{ ref('fact_locations') }} fl ON hc.location_id = fl.location_id
+FROM {{ ref('stg_hospital_charges') }} hc
+JOIN {{ ref('dim_cpt_codes') }} dc ON hc.cpt_id = dc.cpt_id
+JOIN {{ ref('dim_payers') }} dp ON hc.payer_id = dp.payer_id
+JOIN {{ ref('dim_hospitals') }} dh ON hc.hospital_id = dh.hospital_id
+JOIN {{ ref('dim_locations') }} dl ON hc.location_id = dl.location_id
 {% if is_incremental() %}
 WHERE hc.id > (SELECT COALESCE(MAX(id), 0) FROM {{ this }})
 {% endif %}
